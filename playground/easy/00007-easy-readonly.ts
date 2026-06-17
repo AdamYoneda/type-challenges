7e1478a332dbb97fe0a17d670aacc1e74f2b5d54
Adam Yoneda <adam.yoneda.developer@gmail.com>
2026-06-17 18:24:11 +0900

    chore(easy 2/n): 00007

--- playground/easy/00007-easy-readonly.ts
+++ playground/easy/00007-easy-readonly.ts
@@ -31,7 +31,10 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyReadonly<T> = any
+type MyReadonly<T> = { readonly [P in keyof T]: T[P]}
+// type MyReadonly<T> = { +readonly [P in keyof T]: T[P]} // +, - mapping modifier（明示的に付ける | 外す）
+// type MyReadonly<T> = readonly {[P in keyof T]: T[P]} // 'readonly' type modifier is only permitted on array and tuple literal types.
+// type MyReadonly<T> = {[P in keyof T]: readonly T[P]} // 'readonly' type modifier is only permitted on array and tuple literal types.
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -54,4 +57,34 @@ interface Todo1 {
   > Share your solutions: https://tsch.js.org/7/answer
   > View solutions: https://tsch.js.org/7/solutions
   > More Challenges: https://tsch.js.org
+*/
+
+
+// readonly operator
+// https://www.typescriptlang.org/docs/handbook/2/classes.html#readonly
+// `readonly`は使える箇所が文脈によって異なる
+/*
+  ---
+  readonly が使える場所は3つ
+
+  1. オブジェクト型のプロパティ修飾子
+
+  { readonly [P in keyof T]: T[P] }  // OK（34行目）
+
+  readonly を 個々のプロパティの前 に付ける。これが Mapped Type で使う正しい位置。
+
+  2. 配列・タプルのリテラル型
+
+  readonly string[]        // OK → ReadonlyArray<string>
+  readonly [string, number] // OK → readonly タプル
+
+  エラーメッセージの「only permitted on array and tuple literal types」はこちらを指している。
+
+  3. クラスのプロパティ宣言
+
+  class Foo {
+    readonly name: string  // OK
+  }
+
+  ---
 */
